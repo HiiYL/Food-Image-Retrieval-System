Fixed average precision implementation

computeFeatures.py
@@ -8,11 +8,28 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-from cyvlfeat import sift as cysift
+
+from squeezenet import get_squeezenet
+
+width = 227
+height = 227
+channel = 3
+
+# from cyvlfeat import sift as cysift
 
 # you are allowed to import other Python packages above
 ##########################
-def computeFeatures(img):
+
+def preprocessImage(image):
+    im = cv2.resize(image, (width, height)).astype(np.float32)
+    im[:,:,0] -= 103.939
+    im[:,:,1] -= 116.779
+    im[:,:,2] -= 123.68
+    im = im.transpose((2,0,1))
+    im = np.expand_dims(im, axis=0)
+    return im
+
+def computeFeatures(img, deep_features=True):
     # Inputs
     # img: 3-D numpy array of an RGB color image
     #
@@ -23,10 +40,17 @@ def computeFeatures(img):
     # ADD YOUR CODE BELOW THIS LINE
     
     # This is the baseline method: 192-D RGB colour feature histogram
-    rhist, rbins = np.histogram(img[:,:,0], 64, normed=True)
-    ghist, gbins = np.histogram(img[:,:,1], 64, normed=True)
-    bhist, bbins = np.histogram(img[:,:,2], 64, normed=True)
-    featvect = np.concatenate((rhist, ghist, bhist))
+    if deep_features:
+        processed_img = preprocessImage(img)
+        squeezenet_model = get_squeezenet(nb_classes=10,
+            path_to_weights='model/squeezenet_food.h5',
+            dim_ordering='th')
+        featvect = squeezenet_model.predict(processed_img)
+    else:
+        rhist, rbins = np.histogram(img[:,:,0], 64, normed=True)
+        ghist, gbins = np.histogram(img[:,:,1], 64, normed=True)
+        bhist, bbins = np.histogram(img[:,:,2], 64, normed=True)
+        featvect = np.concatenate((rhist, ghist, bhist))
 
     # img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 

preprocess.py
@@ -38,30 +38,11 @@ def preprocessImage(image):
     return im
 
 
-squeezenet_model = get_squeezenet(nb_classes=10,
- path_to_weights='model/squeezenet_weights_th_dim_ordering_th_kernels.h5',
- dim_ordering='th')
-
-adam = Adam(lr=0.0001,clipnorm=1.,clipvalue=0.5)
-squeezenet_model.compile(optimizer=adam,loss='categorical_crossentropy', metrics=['accuracy'])#,loss_weights=[1., 0.2])
-
-# from keras.utils.visualize_util import plot
-# plot(squeezenet_model, to_file='{}.png'.format('squeezenet'),show_shapes=True)
-
-squeezenet_model.fit(data, Y_train, nb_epoch=5, batch_size=32)
-
 # EDIT THIS TO YOUR OWN PATH IF DIFFERENT
 dbpath = 'fooddb/'  
 
 # these labels are the abbreviations of the actual food names
 labels = ('AK','BL','CD','CL','DR','MG','NL','PG','RC','ST')
-
-y = np.array([ [i] * 100 for i in range(10)]).flatten()
-
-Y_train = to_categorical(y, 10)
-
-
-
 
 
 featvect = []  # empty list for holding features
@@ -82,35 +63,32 @@ else:
 
     np.save('images.npy' ,data)
 
+if not os.path.isfile('model/squeezenet_food.h5'):
+    y = np.array([ [i] * 100 for i in range(10)]).flatten()
+    Y_train = to_categorical(y, 10)
+    squeezenet_model = get_squeezenet(nb_classes=10,
+     path_to_weights='model/squeezenet_weights_th_dim_ordering_th_kernels.h5',
+     dim_ordering='th')
+
+    adam = Adam(lr=0.0001,clipnorm=1.,clipvalue=0.5)
+    squeezenet_model.compile(optimizer=adam,loss='categorical_crossentropy', metrics=['accuracy'])#,loss_weights=[1., 0.2])
+
+    # from keras.utils.visualize_util import plot
+    # plot(squeezenet_model, to_file='{}.png'.format('squeezenet'),show_shapes=True)
+
+    squeezenet_model.fit(data, Y_train, nb_epoch=10, batch_size=32)
+    squeezenet_model.save_weights('model/squeezenet_food.h5')
+else:
+    squeezenet_model = get_squeezenet(nb_classes=10,
+     path_to_weights='model/squeezenet_food.h5',
+     dim_ordering='th')
+
 e1 = cv2.getTickCount()
 featvect = squeezenet_model.predict(data)
 e2 = cv2.getTickCount()
 
 FEtime = (e2 - e1) /cv2.getTickFrequency() 
 print('Feature extraction runtime: %.4f seconds'%np.sum(FEtime))
-
-
-
-    
-    # display image
-    #plt.imshow(img), plt.xticks([]), plt.yticks([])
-    #plt.show()
-    
-    # # compute features and append to list
-    # e1 = cv2.getTickCount() # start timer
-    # feat = computeFeatures(img, model=squeezenet_model)
-    # e2 = cv2.getTickCount()  # stop timer
-    
-    # featvect.append( feat ); 
-    # FEtime[idx] = (e2 - e1) / cv2.getTickFrequency() 
-    
-    # print('Extracting features for image #%d'%idx )
-
-
-
-# temparr = np.array(featvect)
-# fv = np.reshape(temparr, (temparr.shape[0], temparr.shape[1]) )
-# del temparr
 
 # pickle your features
 pickle.dump( featvect, open( "feat.pkl", "wb" ) )